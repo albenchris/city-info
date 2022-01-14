32951fb86e4d37e3e361afb6736ea6da6af7aff7
albenchris <albenchris00@gmail.com>
2022-01-14 14:35:15 -0600

    function to request coarse location permission from user

--- app/src/main/java/com/example/cityinfo/MainActivity.kt
+++ app/src/main/java/com/example/cityinfo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.cityinfo
 
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var searchCityName : SearchView
 //    lateinit var buttonSearch : Button
+
+//    val LOCATION_FINE_PERMISSION_CODE : Int = 1
+    val LOCATION_COARSE_PERMISSION_CODE : Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +50,49 @@ class MainActivity : AppCompatActivity() {
 //            getLocationInfo()
 //        }
 
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+        ) {
+            // do some stuff
+            requestPermission(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                LOCATION_COARSE_PERMISSION_CODE,
+                "Using location will provide closest searched location in case of duplicates"
+            )
+        }
+    }
+
+    fun requestPermission(
+        requestedPermission: String,
+        PERMISSION_CODE: Int,
+        message: String
+    ) {
+        if (ActivityCompat
+                .shouldShowRequestPermissionRationale(this,
+                    requestedPermission
+                )
+        ) {
+            AlertDialog.Builder(this)
+                .setTitle("Permission needed")
+                .setMessage(message)
+                .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                    ActivityCompat.requestPermissions(this,
+                        Array<String>(1) { requestedPermission },
+                        PERMISSION_CODE
+                    )
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                    dialog.dismiss()
+                })
+                .create()
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                Array<String>(1) { requestedPermission },
+                PERMISSION_CODE
+            )
+        }
     }
 
     private fun getLocationInfo(cityName: String?) {
